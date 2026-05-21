1c714b4139c2be44341d7461f1532b2ac8d72f9c
PALICOU <palicou@macbookpro.home>
2026-05-21 10:17:34 +0200

    fix supabase SSR crash

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+// ❗ IMPORTANT: on ne crée le client que côté browser
+export const supabase =
+  typeof window !== "undefined"
+    ? createClient(supabaseUrl, supabaseAnonKey)
+    : null;
